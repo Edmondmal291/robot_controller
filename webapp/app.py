ad280b90a3a6ca227523e6615d6c11994acd2d43
Edmond <edmondmal291@gmail.com>
2026-05-31 19:11:33 -0500

    intial commit

---
+++ webapp/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from services.motors import Motors
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/cakes')
+def cakes():
+    return 'Yummy cakes !'
+
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template('page.html',name=name)
+
+@app.route('/forward')
+def forward():
+    motors = Motors()
+    motors.move_forward()
+    return {"status":"Success","message":"Data recieved, moving forward"}
+
+@app.route('/backward')
+def backward():
+    motors = Motors()
+    motors.move_backward()
+    return {"status":"Success","message":"Data recieved, moving backward"}
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')  
---
+++ webapp/services/motors.py
@@ -0,0 +1,14 @@
+from gpiozero import Motor
+class Motors():
+
+    def __init__(self):
+        self.motor = Motor(17,18)
+
+    def move_forward(self):
+        self.motor.forward()
+
+    def move_backward(self):
+        self.motor.backward()
+
+    def stop_motor(self):
+        self.motor.stop()
